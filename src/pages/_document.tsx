5ea36c2c85698b7c25131d6d02b3907591eef7f9
Luiz <l.bueno@xsensors.ai>
2023-05-28 15:16:17 -0300

    chore: config global css and eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
+import { getCssText, theme } from '@luiz504-ignite-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" style={{ backgroundColor: theme.colors.gray900.value }}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 
-
-const inter = Inter({ subsets: ['latin'] })
+import { Heading } from '@luiz504-ignite-ui/react'
 
 export default function Home() {
   return (
@@ -13,9 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <main className={`${inter.className}`}>
-        Hello Word
+
+      <main>
+        <Heading as={'h1'}>Hello Word</Heading>
       </main>
     </>
   )
